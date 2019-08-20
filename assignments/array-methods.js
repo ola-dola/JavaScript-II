6254be3bb2a718728a853284abecba881c08594a
oredola <oredolaolamide@gmail.com>
2019-08-20 13:36:32 +0100

    Completed Challenge 4 using the .reduce method

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -90,7 +90,9 @@ console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
-let ticketPriceTotal = 0;
+let ticketPriceTotal = runners.reduce((total, company) => {
+  return total + company.donation;
+}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
