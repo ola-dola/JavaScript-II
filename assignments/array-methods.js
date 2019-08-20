f6e01d6e6eb6e2ce2b418e61f6a0981332a0b0fa
oredola <oredolaolamide@gmail.com>
2019-08-20 12:52:01 +0100

    Solved challenge 2 using the map method

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -59,13 +59,22 @@ const runners = [
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
 let fullNames = [];
 runners.forEach(names => {
-  fullNames.push(`${names.first_name} ${names.first_name}`)
+  fullNames.push(`${names.first_name} ${names.last_name}`)
 });
 console.log(fullNames);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
 let firstNamesAllCaps = [];
+
+function allCaps(fNames) {
+  return fNames.first_name.toUpperCase();
+}
+firstNamesAllCaps = runners.map(allCaps);
+
+//Short way
+// firstNamesAllCaps = runners.map( fNames => fNames.first_name.toUpperCase() );
+
 console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
